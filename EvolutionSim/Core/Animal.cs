d39f395d7626637ad46d64b2d43b6d9b2b6e1a6a
Simon Lauser <simon@lauser.dev>
2024-01-27 12:45:50 +0100

    Add brain and eye to animal

--- EvolutionSim/Core/Animal.cs
+++ EvolutionSim/Core/Animal.cs
@@ -1,11 +1,28 @@
-﻿using GeneticWorld.Model;
+﻿using EvolutionSim.Core;
+using GeneticWorld.Model;
 using GeometRi;
 
 namespace GeneticWorld.Core;
 
-public class Animal(IRandomGenerator rng)
+public class Animal
 {
-    public Point3d Position { get; set; } = new(rng.GetRandomNumberInRange(0, 1), rng.GetRandomNumberInRange(0, 1), 0);
-    public Rotation Rotation { get; set; } = Rotation.FromEulerAngles(0, 0, rng.GetRandomNumberInRange(0, Math.PI * 2), "xyz");
-    public double Speed { get; set; } = 0.002;
+    public Point3d Position { get; set; }
+    public Rotation Rotation { get; set; }
+    public double Speed { get; set; } = 0.005;
+    public Eye Eye { get; set; }
+    public Network Brain { get; set; }
+
+    public List<double> ProcessVision(IReadOnlyList<Food> foods) => Eye.ProcessVision(Position, Rotation, foods);
+
+    public Animal(IRandomGenerator rng)
+    {
+        Position = new(rng.GetRandomNumberInRange(0, 1), rng.GetRandomNumberInRange(0, 1), 0);
+        Rotation = Rotation.FromEulerAngles(0, 0, rng.GetRandomNumberInRange(0, Math.PI * 2), "xyz");
+
+        Eye = new Eye();
+
+        List<LayerTopology> topology = [new LayerTopology(Eye.Cells), new LayerTopology(2 * Eye.Cells), new LayerTopology(2)];
+
+        Brain = new(rng, topology);
+    }
 }
--- EvolutionSim/Core/Simulation.cs
+++ EvolutionSim/Core/Simulation.cs
@@ -5,12 +5,33 @@ namespace GeneticWorld.Core;
 
 public class Simulation(IRandomGenerator rng)
 {
+    double SpeedMin { get; set; } = 0.001;
+    double SpeedMax { get; set; } = 0.005;
+    double SpeedAccel { get; set; } = 0.2;
+    double RotationAccel { get; set; } = Math.PI / 2.0;
+
     public World World { get; } = new(rng);
 
     public void step()
     {
         ProcessCollisions();
+        ProcessBrain();
         ProcessMovements();
+    }
+
+    private void ProcessBrain()
+    {
+        foreach (var animal in World.Animals)
+        {
+            var vision = animal.ProcessVision(World.Foods);
+            var response = animal.Brain.Propagate(vision);
+
+            var speed = Math.Clamp(response[0], -SpeedAccel, SpeedAccel);
+            var rotation = Math.Clamp(response[1], -RotationAccel, RotationAccel);
+
+            animal.Speed = Math.Clamp(animal.Speed + speed, SpeedMin, SpeedMax);
+            animal.Rotation = Rotation.FromEulerAngles(0, 0, animal.Rotation.ToAngle + rotation, "xyz");
+        }
     }
 
     private void ProcessMovements()
